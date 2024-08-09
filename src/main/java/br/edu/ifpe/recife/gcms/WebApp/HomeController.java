f01ab21acec34df97a2c0c9c8b49459d9668df78
Mateus <imaath2@gmail.com>
2024-08-09 11:21:28 -0300

    1o commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,29 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
+	
+	@RequestMapping("/Mateus")
+	public @ResponseBody String greetingFulano() {
+		return "Hello, Mateus!";
+	}
+	
+	/**@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+		.andDo(print()).andExpect(status().isOk())
+		.andExpect(content()
+		.string(containsString("Hello, World")));
+	}
+
+**/
+}
